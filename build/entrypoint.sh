5e4581753aa0b06f74595c6806506df6f968b385
Tri Van Pham <pvtri96@gmail.com>
2023-03-16 16:08:44 +0700

    feat: change base image to debian and remove custom vpnc-script

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,9 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 sed "s/^Port .*$/Port 8888/" -i /etc/tinyproxy.conf
 /usr/bin/tinyproxy -c /etc/tinyproxy.conf
-
-/usr/local/bin/microsocks -i 0.0.0.0 -p 8889 & 
 
 run () {
   # Start openconnect
